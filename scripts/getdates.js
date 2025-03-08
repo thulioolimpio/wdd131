d2c9726e6db34e544b169199e1ee316efb0cb81a
Thulio Olimpio <167149365+thulioolimpio@users.noreply.github.com>
2025-03-08 11:45:24 -0300

    Initial commit

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Obtendo o ano atual para o rodapé
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Obtendo a data da última modificação do documento
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
