04dfb0797ddb15a624ca65be59c4ea1597f4eb84
Thulio Olimpio <167149365+thulioolimpio@users.noreply.github.com>
2025-03-14 21:45:39 -0300

    att

--- week1/scripts/getdates.js
+++
@@ -1,5 +0,0 @@
-// Obtendo o ano atual para o rodapé
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// Obtendo a data da última modificação do documento
-document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
